742a5c1927ba7ea59c9f4e9cd72ea5fc06b1f0c4
haguy77 <idelchikhaguy@gmail.com>
2024-07-13 19:38:46 +0300

    change indexing logic in splitting and removing really short documents

--- src/index_creator.py
+++ src/index_creator.py
@@ -6,11 +6,13 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 class IndexCreator:
     def __init__(self):
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=250)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=250,
+                                                            separators=["\n\n", "\n", "\t"])
         with open("config.yaml", "r") as f:
             config = yaml.safe_load(f)
         self.embeddings = HuggingFaceEmbeddings(model_name=config["embedding_model"])
 
     def create_faiss_index(self, documents):
-        texts = self.text_splitter.split_documents(documents)
+        texts = [text for text in self.text_splitter.split_documents(documents) if
+                 len(text.page_content.split(" ")) > 3]
         return FAISS.from_documents(texts, self.embeddings), texts
